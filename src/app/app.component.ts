b8afb9bbf7b43ec34848d51c4ba416f50bfcec6d
Yuri Timafejn <timafejn@gmail.com>
2023-11-20 20:32:59 -0300

    feat: included collor palete and removed html generated automatic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MainMenuComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
