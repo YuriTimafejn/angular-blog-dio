1600841f8d7fab402cd0a64a4d05e0ce02696c31
Yuri Timafejn <timafejn@gmail.com>
2023-11-20 20:34:21 -0300

    feat: generated basics components to the project

---
+++ src/app/components/main-banner/main-banner.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-main-banner',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './main-banner.component.html',
+  styleUrl: './main-banner.component.scss'
+})
+export class MainBannerComponent {
+
+}
