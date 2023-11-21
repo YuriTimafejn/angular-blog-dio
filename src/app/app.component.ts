08e5c62b79f65ddb73db635fe05f03eac732c668
Yuri Timafejn <timafejn@gmail.com>
2023-11-20 21:31:40 -0300

    feat: created and styled banner component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,20 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { MainBannerComponent } from './components/main-banner/main-banner.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MainMenuComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MainMenuComponent,
+    MainBannerComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
